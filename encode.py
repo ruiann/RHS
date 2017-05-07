e312c2aeb0961139d58c3955be4d97462b6935fa
inverse <534676033@qq.com>
2017-05-07 12:27:32 +0800

    add encode test

---
+++ encode.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+import time
+from RHS import RHS
+from read_data import Data
+
+segment_per_sample = 1000
+segment_length = 100
+channel = 3
+test_count = 30
+test_period = 30
+
+log_dir = './log'
+model_dir = './model'
+
+data = Data(segment_per_sample, segment_length)
+rhs = RHS(lstm_size=800, class_num=data.class_num())
+
+
+def test():
+    data.init_test_data()
+    x = tf.placeholder(tf.float32, shape=(test_count, segment_length, channel))
+    lstm_code = tf.reduce_sum(rhs.lstm(x, test_count, segment_length), 0)
+    tf.summary.histogram('lstm_encode', lstm_code)
+
+    sess = tf.Session()
+    summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
+    summary = tf.summary.merge_all()
+
+    with sess.as_default():
+        sess.run(tf.global_variables_initializer())
+
+        saver = tf.train.Saver()
+        checkpoint = tf.train.get_checkpoint_state(model_dir)
+
+        if checkpoint:
+            saver.restore(sess, checkpoint.model_checkpoint_path)
+
+        start_time = time.time()
+
+        base_lstm = 0;
+
+        for period in xrange(test_period):
+            sample = data.get_segments_for_each_writer(test_count)
+            writer_sample = sample[period]
+            lstm, summary_str = sess.run([lstm_code, summary], feed_dict={x: writer_sample})
+            summary_writer.add_summary(summary_str, period)
+            if period > 0:
+                differ = lstm - base_lstm
+                dis = tf.reduce_mean(tf.square(differ))
+                dis = sess.run(dis)
+                print(dis)
+            base_lstm = lstm
+
+        print("time cost: {0}".format(time.time() - start_time))
+
+
+if __name__ == '__main__':
+    test()
